070cc9a9c4ea4b4b7d3620b0323140aef4986f0e
MasamioNakada <mnakadab@unac.edu.pe>
2022-07-24 17:33:47 -0500

    loader_in_object

--- load.py
+++ load.py
@@ -1,36 +1,39 @@
 from chardet import detect
 import os
 from csv import Sniffer
+import warnings
 
 import pandas as pd
-
-def get_delimiter(path) -> str:
-    white_list = [',', ';', ':', '|', '\t']
-    if path[-3:] == 'csv':
-        with open(path,'r') as file:
-            firstline = file.readline()
-            for sep in firstline:
-                if sep in white_list:
-                    return sep
-            file.close()                      
-
-def get_encoding(path):
-    raw = open(path,'rb').read()
-    encod = detect(raw)
-    return encod['encoding']
-
-
 
 class Load:
     def __init__(self, path:str):
         self.path = path
-        self.labels = os.listdir(self.path)
+        self.labels = os.listdir(self.path) 
 
+    def get_encoding(self,full_path) -> str:
+        raw = open(full_path,'rb').read()
+        encod = detect(raw)
+        return encod['encoding']
+
+    def get_delimiter(self,full_path) -> str:
+        white_list = [',', ';', ':', '|', '\t']
+        if self.path[-3:] == 'csv':
+            with open(full_path, mode='r',encoding=self.get_encoding(full_path)) as file:
+                firstline = file.readline()
+                for sep in firstline:
+                    if sep in white_list:
+                        print(sep)
+                file.close()  
+        
     def load_from_csv(self):
         data_dict = {}
+
         for name in self.labels:
-            full_path = f'./{self.path}/{name}'
-            data_dict[name[:-4]] = pd.read_csv(full_path,sep=get_delimiter(full_path),encoding=get_encoding(full_path))
-            return data_dict
+            if name[-3:] == 'csv': 
+                full_path = f'./{self.path}/{name}'
+                data_dict[name[:-4]] = pd.read_csv(full_path,sep=self.get_delimiter(full_path),encoding=self.get_encoding(full_path),engine='python')
+            else:
+                warnings.warn(message = f'{name} is not a csv file, ingoring', category=UnicodeWarning, stacklevel=2)
+        return data_dict
 
 
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from load import Load
 load = Load('in')
 if __name__ == '__main__':
-    #print(delimiter('./in/Compra.csv'))
-    #print(encoding('./in/Compra.csv'))
     data_dict = load.load_from_csv()
-    print(data_dict)
-    #print(data_dict.keys())
+    print(data_dict.keys())
 
 
